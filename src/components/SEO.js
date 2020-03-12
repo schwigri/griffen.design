3eaee6f64dd17cb97ea339f8488bb8ac76a7ecd7
Griffen Schwiesow <schwigri@pm.me>
2020-03-12 16:51:16 -0700

    🧙‍♂️ Add cover image

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
+import { graphql, useStaticQuery } from 'gatsby';
 
 function SEO({ title, titleSuffix, description }) {
 	const pageTitle = `${title}${titleSuffix}`;
+
+	const data = useStaticQuery(
+		graphql`
+			query SeoQuery {
+				datoCmsSite {
+					globalSeo {
+						fallbackSeo {
+							image {
+								url
+							}
+						}
+					}
+				}
+			}
+		`
+	);
+
+	const defaultCover = data.datoCmsSite.globalSeo.fallbackSeo.image.url;
 
 	return (
 		<Helmet>
 			<title>{pageTitle}</title>
 			<meta name="description" content={description} />
+			<meta property="og:image" content={defaultCover} />
 		</Helmet>
 	);
 }
