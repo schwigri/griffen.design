b0219d6b02d61bfa8b0a76bf6ab934d9e170a106
Griffen Schwiesow <schwigri@pm.me>
2020-04-18 23:02:44 -0700

    😥 Bye, Mastodon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -148,19 +148,6 @@ function Footer({ locale, id = '', variants }) {
 						</abbr>
 					</a>
 				</span>
-				<span className="privacy-link-container mastodon-link">
-					<a
-						href="https://pnw.life/@gs"
-						rel="me noopener noreferrer"
-						target="_blank"
-						title="@gs on Mastodon"
-					>
-						<span className="sr-only">@gs on Mastodon (pnw.life)</span>
-						<span className="emoji" role="img" aria-hidden="true">
-							🐘
-						</span>
-					</a>
-				</span>
 			</div>
 
 			<div className="locale-select">
